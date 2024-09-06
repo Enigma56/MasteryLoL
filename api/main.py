4afad43f5a830f58a27cd1b198f3068fa9a5fde1
Charlie Lyster <saltystudiosinc@gmail.com>
2024-09-06 19:51:37 -0400

    Change file structure for api modules

--- api/main.py
+++ api/main.py
@@ -1,5 +1,9 @@
 from flask import Flask
 
-app = Flask('api')
-app.run(debug=True)
+def hello_world():
+    print("Hello!")
+
+if __name__ == "__main__":
+    app = Flask('api')
+    app.run(debug=True)
 
---
+++ test.py
@@ -0,0 +1,6 @@
+from api.main import hello_world
+
+hello_world()
+
+if __name__ == "main":
+    print(__name__)
