9e09494b0fb2cc3fa593b9a0dbccd471b12a065a
Charlie Lyster <saltystudiosinc@gmail.com>
2024-09-06 20:18:54 -0400

    Get Riot Account puuid and other information

--- api/account_data.py
+++ api/account_data.py
@@ -1,2 +1,10 @@
 import requests
+import os
 
+def get_account(name: str, tagline: str) -> dict[str, str]: 
+    API_KEY: str | None = os.environ.get("API_KEY")
+    url: str = f"https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/{name}/{tagline}" 
+    r = requests.get(url, headers={"Content-Type": "application/json", "X-Riot-Token": f"{API_KEY}"})
+    dat = r.json()
+    print(dat)
+    return dat
--- api/main.py
+++ api/main.py
@@ -1,9 +1,14 @@
 from flask import Flask
+from dotenv import load_dotenv
+import account_data
 
 def hello_world():
     print("Hello!")
 
 if __name__ == "__main__":
+    load_dotenv()
+    account_data.get_account("Its%20Just%20A%20Prank", "6969")
+
     app = Flask('api')
     app.run(debug=True)
 
