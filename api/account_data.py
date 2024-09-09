564ffaa9429000e89ffdf9033387a52ca8c5debe
Charlie Lyster <saltystudiosinc@gmail.com>
2024-09-09 15:30:48 -0400

    Reorganize files to create working test suite boiler plate code

--- api/account_data.py
+++ api/account_data.py
@@ -1,10 +1,13 @@
-import requests
+from typing import Tuple
 import os
+import requests
 
-def get_account(name: str, tagline: str) -> dict[str, str]: 
-    API_KEY: str | None = os.environ.get("API_KEY")
-    url: str = f"https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/{name}/{tagline}" 
-    r = requests.get(url, headers={"Content-Type": "application/json", "X-Riot-Token": f"{API_KEY}"})
-    dat = r.json()
-    print(dat)
-    return dat
+
+def get_account(name: str, tagline: str) -> Tuple[int, dict[str, str]]:
+    APIKEY: str | None = os.environ.get("API_KEY")
+    url: str = f"https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/{name}/{tagline}"
+    r = requests.get(url, headers={"Content-Type": "application/json", "X-Riot-Token": f"{APIKEY}"})
+    account_info = r.json()
+    # print(account_info)
+    r.close()
+    return r.status_code, account_info
