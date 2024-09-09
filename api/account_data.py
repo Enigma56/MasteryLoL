1c8d76ed4997e992ac7879b806c354c5517ef785
Charlie Lyster <saltystudiosinc@gmail.com>
2024-09-09 15:44:21 -0400

    Clean up get_account function

--- api/account_data.py
+++ api/account_data.py
@@ -4,10 +4,18 @@ import requests
 
 
 def get_account(name: str, tagline: str) -> Tuple[int, dict[str, str]]:
-    APIKEY: str | None = os.environ.get("API_KEY")
-    url: str = f"https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/{name}/{tagline}"
-    r = requests.get(url, headers={"Content-Type": "application/json", "X-Riot-Token": f"{APIKEY}"})
-    account_info = r.json()
+    """
+    Retrieves Riot Account information with the associated IGN and tagline
+    """
+    api_key: str | None = os.environ.get("API_KEY")
+    url: str = f"""https://americas.api.riotgames.com/riot/account/v1/
+                               accounts/by-riot-id/{name}/{tagline}"""
+    req = requests.get(
+            url,
+            timeout=10,
+            headers={"Content-Type": "application/json",
+                                     "X-Riot-Token": f"{api_key}"})
+    account_info = req.json()
     # print(account_info)
-    r.close()
-    return r.status_code, account_info
+    req.close()
+    return req.status_code, account_info
