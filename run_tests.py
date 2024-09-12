c5c7a461798ba3cc624c4e98e2851da42b224bad
Charlie Lyster <saltystudiosinc@gmail.com>
2024-09-11 21:12:12 -0400

    Add another function and a test for retrieval of summoner information. Started db folder for db connections

--- run_tests.py
+++ run_tests.py
@@ -7,8 +7,6 @@ if __name__ == "__main__":
     print('Using Python version {0!s}'.format(sys.version))
     print("Loading Environment variables")
     load_dotenv()
-    #app = Flask(__name__)
-    #app.run(debug=True)
 
     test_suite = unittest.TestLoader().discover('test')
     test_results = unittest.TextTestRunner(verbosity=2).run(test_suite)
--- test/test_accounts.py
+++ test/test_accounts.py
@@ -1,11 +1,17 @@
 import unittest
 
-from api.account_data import get_account
+from api.account_data import get_riot_puuid, get_summoner_information
 # TODO: Consider using pytest for its fixtures instead
 
 
 class TestAccount(unittest.TestCase):
-    def test_get_account(self):
-        status_code, _ = get_account("Its Just A Prank", "6969")
+    def test_get_puuid(self):
+        status_code, puuid = get_riot_puuid("Its Just A Prank", "6969")
         self.assertLess(status_code, 400)
-        # self.assertIsNotNone(account_info)
+        self.assertIsNotNone(puuid)
+
+    def test_get_summoner_information(self):
+        _, puuid = get_riot_puuid("Its Just A Prank", "6969")
+        status_code, info = get_summoner_information(puuid)
+        self.assertLess(status_code, 400)
+        self.assertIsNotNone(info)
