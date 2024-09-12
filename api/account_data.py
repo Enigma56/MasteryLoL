f009258b9d76bc3bd565f72a4d808ef6016d935d
Charlie Lyster <saltystudiosinc@gmail.com>
2024-09-11 21:12:12 -0400

    Add another function and a test for retrieval of summoner information. Started db folder for db connections

--- api/account_data.py
+++ api/account_data.py
@@ -1,21 +1,46 @@
-from typing import Tuple
+from typing import Tuple, Final
 import os
 import requests
 
 
-def get_account(name: str, tagline: str) -> Tuple[int, dict[str, str]]:
+API_KEY: str | None = os.environ.get("API_KEY")
+ACCOUNT_TIMEOUT: Final[int] = 5
+
+
+def get_riot_puuid(name: str, tagline: str) -> Tuple[int, str]:
     """
     Retrieves Riot Account information with the associated IGN and tagline
     """
-    api_key: str | None = os.environ.get("API_KEY")
-    url: str = f"""https://americas.api.riotgames.com/riot/account/v1/
-                               accounts/by-riot-id/{name}/{tagline}"""
+    base_url: str = "https://americas.api.riotgames.com"
+    endpoint: str = f"/riot/account/v1/accounts/by-riot-id/{name}/{tagline}"
+    url: str = f"{base_url}{endpoint}"
     req = requests.get(
             url,
-            timeout=10,
+            timeout=ACCOUNT_TIMEOUT,
             headers={"Content-Type": "application/json",
-                                     "X-Riot-Token": f"{api_key}"})
+                     "X-Riot-Token": f"{API_KEY}"
+                     }
+            )
     account_info = req.json()
     # print(account_info)
     req.close()
-    return req.status_code, account_info
+    return req.status_code, account_info["puuid"]
+
+
+def get_summoner_information(riot_puuid: str) -> Tuple[int, dict[str, str]]:
+    """
+    Retrieves summoner information from a provided Riot PUUID
+    """
+    base_url: str = "https://na1.api.riotgames.com"
+    endpoint: str = f"/lol/summoner/v4/summoners/by-puuid/{riot_puuid}"
+    url: str = f"{base_url}{endpoint}"
+    req = requests.get(
+            url,
+            timeout=ACCOUNT_TIMEOUT,
+            headers={"Content-Type": "application/json",
+                     "X-RIOT-Token": f"{API_KEY}"
+                     }
+            )
+    summoner_info = req.json()
+
+    return req.status_code, summoner_info
--- api/main.py
+++ api/main.py
@@ -1,11 +1,5 @@
 from flask import Flask
-import account_data
-
-from dotenv import load_dotenv
 
 if __name__ == "__main__":
-    load_dotenv()
-    account_data.get_account("Its Just A Prank", "6969")
-
     app = Flask('api')
     app.run(debug=True)
