157e36e70c968071b51e918bf9f0aee88eb9d009
Charlie Lyster <saltystudiosinc@gmail.com>
2024-09-15 16:01:55 -0400

    Implement the three main endpoints to retrieve player mastery

--- api/mastery.py
+++ api/mastery.py
@@ -3,12 +3,44 @@ import os
 import requests
 
 API_KEY: str | None = os.environ.get("API_KEY")
-BASE_URL: Final[str] = "https://na1.api.riotgames.com"
+BASE_URL: Final[str] = "https://na1.api.riotgames.com/lol/champion-mastery/v4"
 MASTERY_TIMEOUT: Final[int] = 5
 
 
-def get_mastery_by_puuid(riot_puuid: str) -> Tuple[int, dict[str, str]]:
-    endpoint: str = f"/lol/champion-mastery/v4/champion-masteries/by-puuid/{riot_puuid}"
+def get_all_mastery_by_puuid(riot_puuid: str) -> Tuple[int, dict[str, str]]:
+    endpoint: str = f"/champion-masteries/by-puuid/{riot_puuid}"
+    url: str = f"{BASE_URL}{endpoint}"
+    req = requests.get(
+            url,
+            timeout=MASTERY_TIMEOUT,
+            headers={"Content-Type": "application/json",
+                     "X-RIOT-TOKEN": f"{API_KEY}"
+                     }
+            )
+    mastery_info = req.json()
+    return req.status_code, mastery_info
+
+
+def get_top_mastery_by_puuid(riot_puuid: str) -> Tuple[int, dict[str, str]]:
+    endpoint: str = f"/champion-masteries/by-puuid/{riot_puuid}/top"
+    url: str = f"{BASE_URL}{endpoint}"
+    req = requests.get(
+            url,
+            timeout=MASTERY_TIMEOUT,
+            headers={"Content-Type": "application/json",
+                     "X-RIOT-TOKEN": f"{API_KEY}"
+                     }
+            )
+    mastery_info = req.json()
+    return req.status_code, mastery_info
+
+
+def get_sum_mastery_by_puuid(riot_puuid: str) -> Tuple[int, dict[str, str]]:
+    """
+    Get a player's total champion mastery score, which is the sum of
+    individual champion mastery levels.
+    """
+    endpoint: str = f"/scores/by-puuid/{riot_puuid}"
     url: str = f"{BASE_URL}{endpoint}"
     req = requests.get(
             url,
--- test/test_mastery.py
+++ test/test_mastery.py
@@ -1,11 +1,23 @@
 import unittest
-from api.mastery import get_mastery_by_puuid
+import api.mastery as mastery
 from api.account_data import get_riot_puuid
 
 
 class TestMastery(unittest.TestCase):
-    def test_mastery_by_puuid(self):
+    def test_all_mastery_by_puuid(self):
         _, puuid = get_riot_puuid("Its Just A Prank", "6969")
-        status_code, mastery = get_mastery_by_puuid(puuid)
+        status_code, mastery_info = mastery.get_all_mastery_by_puuid(puuid)
         self.assertLess(status_code, 400)
-        self.assertIsNotNone(mastery)
+        self.assertIsNotNone(mastery_info)
+
+    def test_top_mastery_by_puuid(self):
+        _, puuid = get_riot_puuid("Its Just A Prank", "6969")
+        status_code, mastery_info = mastery.get_top_mastery_by_puuid(puuid)
+        self.assertLess(status_code, 400)
+        self.assertIsNotNone(mastery_info)
+
+    def test_sum_mastery_by_puuid(self):
+        _, puuid = get_riot_puuid("Its Just A Prank", "6969")
+        status_code, mastery_info = mastery.get_sum_mastery_by_puuid(puuid)
+        self.assertLess(status_code, 400)
+        self.assertIsNotNone(mastery_info)
